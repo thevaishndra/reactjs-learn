ab8e7bf906415189ff74b28c6b77d71c52f9bd93
thevaishndra <chandrav306m@gmail.com>
2024-06-24 22:51:59 +0530

    Update main.jsx

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (//whatever written here, end of the day it is parsed as in file react element
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+const ReactElement = {
+    type: 'a',//type of element, a is anchor tag
+    props: {//it is an object in which we can add as much as properties
+        href: 'https://google.com',//properties of anchor tag
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https:://google.com"></a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    ReactElement
   
 )
