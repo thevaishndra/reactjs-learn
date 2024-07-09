21a6b6836218c99585f4fedbded613ee7d57c43c
thevaishndra <chandrav306m@gmail.com>
2024-07-09 22:18:16 +0530

    onChange

--- 05passwordgen/src/App.jsx
+++ 05passwordgen/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react'
 
-import './App.css'
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -24,8 +24,31 @@ function App() {
 
   return (
     <>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray'>
-      test
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+        type="text"
+        value= {password}
+        className='outline-none w-full py-1 px-3'
+        placeholder="Password"
+        readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+      </div>
+      <div classname="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input type="range" 
+          min={6}
+          max={100}
+          value={length}
+          classname="cursor-pointer"
+          onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1"></div>
+      </div>
     </div>
     </>
   )
