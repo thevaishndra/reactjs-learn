f23673539db0bc1293b10cf03f4be0f9945bdf22
thevaishndra <chandrav306m@gmail.com>
2024-08-06 10:33:07 +0530

    Update InputBox.jsx

--- 06currenConvert/src/components/InputBox.jsx
+++ 06currenConvert/src/components/InputBox.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function InputBox({
     label,
-    
+    amount,
+    onAmountChage,
+    onCurrencyChange,
+    selectCurrency = "usd",
     className = "",
 }) {
    
