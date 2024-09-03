082da8d47ee345a31f79e744c704f35211366320
thevaishndra <chandrav306m@gmail.com>
2024-09-03 13:56:53 +0530

    currency api

--- 06currenConvert/src/hooks/useCurrencyInfo.js
+++ 06currenConvert/src/hooks/useCurrencyInfo.js
@@ -2,12 +2,15 @@ import {useEffect, useState} from 'react'
 /*designing custom hooks*/
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})/*if nothing is coming, then atleast, an empty object is coming*/
+
     useEffect(() => {
-        fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json')
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())/*converting response to json as mainly apis are in string format*/
         .then((res) => setData(res[currency]))/*currency = key*/
         console.log(data);
     }, [currency])/*dependency array */
+    
+    console.log(data);
     return data;
 }
 
