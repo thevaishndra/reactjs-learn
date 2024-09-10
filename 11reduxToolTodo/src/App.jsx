aac5e555ca82e8e8449533fbcf930fb4b8738c08
thevaishndra <chandrav306m@gmail.com>
2024-09-10 14:41:05 +0530

    debug

--- 11reduxToolTodo/src/App.jsx
+++ 11reduxToolTodo/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='p-2 bg-fuchsia-800' >Hello</h1>
+     <h1 className='p-2 text-white text-3xl' >React Redux Toolkit</h1>
+     <AddTodo/>
+     <Todo />
     </>
   )
 }
--- 11reduxToolTodo/src/app/store.js
+++ 11reduxToolTodo/src/app/store.js
@@ -1,3 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+export const store = configureStore({
+    reducer: todoReducer
+})
 
-export const store = configureStore({})
